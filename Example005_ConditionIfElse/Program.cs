1f814d5c99e91dbdb0f7bea55097aedc8c464214
Vladislav <borodulinvladislav@yandex.ru>
2022-08-31 19:11:52 +0500

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "света")
+{
+    Console.WriteLine("Ура, это же СВЕТА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
